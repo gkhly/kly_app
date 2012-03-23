391aa3e322909cd39868c305a4c5567507849cbd
KLY <yaydeepthought@gmail.com>
2012-03-23 19:10:45 +0800

    init kly_app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+KlyApp::Application.config.secret_token = '931839b1f273a1b605e01eedc04a1de4b14939931baa026db9431d2336543f0e3672c866950b9b6dc36a5c264c0a1f2dc608798f990625c829489df4434f9e8d'
