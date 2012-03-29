200ccafef337eea605e5c6e3dbbcc0149b3e6981
KLY <yaydeepthought@gmail.com>
2012-03-29 23:03:41 +0800

    add user spec testing 2

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,10 @@
+#encoding: utf-8
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+ # pending "add some examples to (or delete) #{__FILE__}"
+   before { @user = User.new( name: "张三", email: "zhangsan@abc.com" ) }
+   subject { @user }
+   it { should respond_to( :name ) }
+   it { should respond_to( :email ) }
 end
